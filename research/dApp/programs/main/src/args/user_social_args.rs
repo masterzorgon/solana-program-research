e51bca06fcd7601d829200bf2e143c9a0369723e
Nathan Galindo <nathan.zebedee@gmail.com>
2023-03-23 17:53:50 -0500

    added research

--- research/dApp/programs/main/src/args/user_social_args.rs
+++
@@ -1,8 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[derive(Debug, AnchorSerialize, AnchorDeserialize)]
-pub struct SocialsArgs {
-    pub name: String,
-    pub discord: String,
-    pub twitter: String,
-}
--- research/dApp/programs/main/src/instructions/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod set_user_socials;
-pub mod create_user_socials;
